51db533a50bf68d38e9df504d56ad40d1045525d
Pablo Merino <pablo.perso1995@gmail.com>
2012-08-29 11:36:03 +0200

    Deploy #1

--- app/controllers/system/static_controller.rb
+++ app/controllers/system/static_controller.rb
@@ -1,5 +1,5 @@
 class System::StaticController < ApplicationController
-	before_filter :authenticate_user!, :only => [:about, :api]
+	before_filter :authenticate_user!, :only => [:api]
 	def index
 		if user_signed_in?
 			redirect_to timeline_path
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Bolt::Application.routes.draw do
 
   
 
-	devise_for :users, :controllers => {:registrations => "registrations"}
+	devise_for :users
 
 	resources :statuses, :only => [:create]
 
